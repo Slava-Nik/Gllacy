c6afda164ab9d44b17188f4535ca7178bb0cea3f
«Slava-Nik» <lancerokey@yandex.ru>
2016-07-04 23:30:21 +0300

    Добавил форму обратной связи и интерактивную карту от гугл

--- js/script.js
+++ js/script.js
@@ -5,6 +5,11 @@ var toogle3=document.querySelector("label[for=toogle3]");
 var title1=document.querySelector(".slide1");
 var title2=document.querySelector(".slide2");
 var title3=document.querySelector(".slide3");
+var overlay=document.querySelector(".overlay");
+var form=document.querySelector(".form-feedback");
+var userName=form.querySelector(".user-name");
+var formOpen=document.querySelector(".adress-block a");
+var formClose=form.querySelector(".form-close");
 
 toogle1.addEventListener("click",function(event1) {
   console.log("Клик по переключателю 1");
@@ -35,4 +40,30 @@ toogle3.addEventListener("click",function(event3) {
   title1.classList.add("hidden");
   title2.classList.add("hidden");
   title3.classList.remove("hidden");
+});
+
+formOpen.addEventListener("click",function(open)
+{
+  open.preventDefault();
+  overlay.classList.remove("hidden");
+  form.classList.remove("hidden");
+  userName.focus();
+});
+
+formClose.addEventListener("click",function(close1)
+{
+  form.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
+
+window.addEventListener("keydown",function(close2)
+{
+  if(close2.keyCode===27)
+    {
+      if(!form.classList.contains("hidden"))
+      {
+        form.classList.add("hidden");
+        overlay.classList.add("hidden");
+      }
+    }
 });
